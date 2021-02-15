aab2aca19bdb885fa250dae7b0219ddf6da457ca
Daniel Contreras <34799702+Cuacktality@users.noreply.github.com>
2021-02-15 04:54:56 -0500

    First Commit

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float velZ = 25f;
+    Rigidbody bala;
+    void Start()
+    {
+        bala = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bala.velocity = new Vector3(0, 0, velZ);
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Wall"))
+        {
+            Destroy(gameObject, 0.001f);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5;
+    public float jump = 6;
+    public float gravity = -20;
+
+    private float hr, vr;
+    public FixedJoystick joy;
+
+    Vector3 movIn = Vector3.zero;
+    CharacterController ch;
+
+    public GameObject bala;
+    public GameObject position;
+    Vector3 pos;
+    public float tDisp = 0.5f;
+    float sigDisp = 0.0f;
+
+    public Camera cam;
+    public GameObject camFPS;
+    public GameObject camTPS;
+    bool fps = false;
+
+    private void Awake()
+    {
+        ch = GetComponent<CharacterController>();
+    }
+    private void Update()
+    {
+        hr = joy.Horizontal;
+        vr = joy.Vertical;
+        Movemento();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            JumpIn();
+        }
+    }
+
+    public void Movemento()
+    {
+        if (ch.isGrounded)
+        {
+            movIn = new Vector3(hr, 0f, vr);
+            movIn = transform.TransformDirection(movIn * speed);
+        }
+
+        movIn.y += gravity * Time.deltaTime;
+        ch.Move(movIn * Time.deltaTime);
+    }
+
+    public void JumpIn()
+    {
+        if (ch.isGrounded)
+        {
+            movIn.y = Mathf.Sqrt(jump * -2f * gravity);
+        }
+        movIn.y += gravity * Time.deltaTime;
+        ch.Move(movIn * Time.deltaTime);
+    }
+
+    public void Shoot()
+    {
+        pos = position.transform.position;
+        if (Time.time > sigDisp)
+        {
+            sigDisp = Time.time + tDisp;
+            Instantiate(bala, pos, Quaternion.identity);
+        }
+    }
+
+    public void CameraC()
+    {
+        if (fps == false)
+        {
+            fps = true;
+            cam.transform.position = camFPS.transform.position;
+        }
+        else
+        {
+            fps = false;
+            cam.transform.position = camTPS.transform.position;
+        }
+    }
+}
